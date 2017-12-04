8adea87931d66d73c2118a5700d958f4919cb243
foostan <ks@fstn.jp>
2017-12-05 02:55:38 +0900

    Esc & i keyboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
-#include <Arduino.h>
+#include "Keyboard.h"
 
-void setup() {
-    // put your setup code here, to run once:
+int L_KEY = 3;
+int R_KEY = 2;
+int L_LED = 10;
+int R_LED = 9;
+
+void setup()
+{
+    pinMode(L_KEY, INPUT_PULLUP);
+    pinMode(R_KEY, INPUT_PULLUP);
+    pinMode(L_LED, OUTPUT);
+    pinMode(R_LED, OUTPUT);
+    Keyboard.begin();
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+    delay(100);
+
+    if (digitalRead(L_KEY) == HIGH) {
+        digitalWrite(L_LED, LOW);
+    } else {
+        digitalWrite(L_LED, HIGH);
+        Serial.print('E');
+        Keyboard.press(KEY_ESC);
+    }
+    if (digitalRead(R_KEY) == HIGH) {
+        digitalWrite(R_LED, LOW);
+        digitalWrite(R_LED, LOW);
+    } else {
+        digitalWrite(R_LED, HIGH);
+        Serial.print('i');
+        Keyboard.press('i');
+    }
+    Keyboard.releaseAll();
 }
